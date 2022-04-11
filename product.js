36b63132eb5adc6c82e71f4e63e8c540befa316c
Francis Colendres <flcolendres@myseneca.ca>
2022-04-11 15:20:20 -0400

    adding errors to side panel

--- product.js
+++ product.js
@@ -1,75 +1,81 @@
 function digitOnly() {
-    var result = true;
     var product_id = document.signup.productId.value.trim();
-    for(var i=0; i < product_id.length && result == true; i++) {
+    for(var i=0; i < product_id.length; i++) {
         if(product_id[i] < '0' || product_id[i] > '9'){
             alert("Please enter numbers only on Product ID")
-            result = false;
+            document.querySelector("#error_1").removeAttribute("hidden");
+            return false;
         }    
         else if(product_id.length != 8) {
             alert("Please enter 8 digits on Product ID")
-            result = false;
+            document.querySelector("#error_2").removeAttribute("hidden");
+            return false;
         }
     }
-    return result;
+    document.querySelector("#error_1").setAttribute("hidden", "hidden");
+    document.querySelector("#error_2").setAttribute("hidden", "hidden");
+    return true;
 }
 
 function productDescription() {
-    var result = true;
     var char;
     var product_desc = document.signup.productDesc.value.trim();
     if(product_desc[0] < 'A' || product_desc[0] > 'Z') {
         alert("The product description must start with capital letters");
-        result = false;
+        document.querySelector("#error_3").removeAttribute("hidden");
+        return false;
     }
-    for(var i=0; i < product_desc.length && result == true; i++) {
+    for(var i=0; i < product_desc.length; i++) {
         char = product_desc[i].toUpperCase();
         if(char < 'A' || char > 'Z') {
             alert("The product description can only have alphabets");
-            result = false;
+            document.querySelector("#error_4").removeAttribute("hidden");
+            return false;
         }
     }
-    return result;
+    document.querySelector("#error_3").setAttribute("hidden", "hidden");
+    document.querySelector("#error_4").setAttribute("hidden", "hidden");
+    return true;
 }
  
 function price() {
-    var result = true;
     var price = document.signup.price.value;
     if(price >= 1000) {
         alert("Price should be less than 1000");
-        result = false;
+        document.querySelector("#error_5").removeAttribute("hidden");
+        return false;
     }
-    return result;
+    document.querySelector("#error_5").setAttribute("hidden", "hidden");
+    return true;
 }
 
 function userName() {
-    var result = true;
     var user_name = document.signup.username.value;
     var char = user_name[0].toUpperCase();
     if(char < 'A' || char > 'Z') {
         alert("Username must start with alphabet");
-        result = false;
+        document.querySelector("#error_6").removeAttribute("hidden");
+        return false;
     }
-    return result;
+    document.querySelector("#error_6").setAttribute("hidden", "hidden");
+    return true;
 }
 
 function checkBox() {
-    var result = false;
-    for(var i = 0; i < document.signup.status.length && result == false; i++) {
+    for(var i = 0; i < document.signup.status.length; i++) {
         if(document.signup.status[i].checked == true) {
-            result = true;
+            document.querySelector("#error_7").setAttribute("hidden", "hidden");
+            return true;
         }
     }
-    if(result == false) {
-        alert("Select at least one of the checkboxes.");
-    }
-    return result;
+    alert("Select at least one of the checkboxes.");
+    document.querySelector("#error_7").removeAttribute("hidden");
+    return false;
 }
 
 function check() {
-    var result = true;
-    if(digitOnly() == false || productDescription() == false || price() && userName() && checkBox()) {
-        result = false;
+    if(digitOnly() == false || productDescription() == false || price() == false || userName() == false|| checkBox() == false) {
+        return false;
     }
-    return result;
+    return true;
 } 
