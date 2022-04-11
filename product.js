6e191bb735e5abc40aec883df1cf24f4cb0a4ccd
Francis Colendres <flcolendres@myseneca.ca>
2022-04-11 17:29:26 -0400

    added more designs in css: box shadows. Added animation and requirement section in product.html

--- product.js
+++ product.js
@@ -2,12 +2,10 @@ function digitOnly() {
     var product_id = document.signup.productId.value.trim();
     for(var i=0; i < product_id.length; i++) {
         if(product_id[i] < '0' || product_id[i] > '9'){
-            alert("Please enter numbers only on Product ID")
             document.querySelector("#error_1").removeAttribute("hidden");
             return false;
         }    
         else if(product_id.length != 8) {
-            alert("Please enter 8 digits on Product ID")
             document.querySelector("#error_2").removeAttribute("hidden");
             return false;
         }
@@ -21,14 +19,12 @@ function productDescription() {
     var char;
     var product_desc = document.signup.productDesc.value.trim();
     if(product_desc[0] < 'A' || product_desc[0] > 'Z') {
-        alert("The product description must start with capital letters");
         document.querySelector("#error_3").removeAttribute("hidden");
         return false;
     }
     for(var i=0; i < product_desc.length; i++) {
         char = product_desc[i].toUpperCase();
         if(char < 'A' || char > 'Z') {
-            alert("The product description can only have alphabets");
             document.querySelector("#error_4").removeAttribute("hidden");
             return false;
         }
@@ -41,7 +37,6 @@ function productDescription() {
 function price() {
     var price = document.signup.price.value;
     if(price >= 1000) {
-        alert("Price should be less than 1000");
         document.querySelector("#error_5").removeAttribute("hidden");
         return false;
     }
@@ -53,7 +48,6 @@ function userName() {
     var user_name = document.signup.username.value;
     var char = user_name[0].toUpperCase();
     if(char < 'A' || char > 'Z') {
-        alert("Username must start with alphabet");
         document.querySelector("#error_6").removeAttribute("hidden");
         return false;
     }
@@ -68,7 +62,6 @@ function checkBox() {
             return true;
         }
     }
-    alert("Select at least one of the checkboxes.");
     document.querySelector("#error_7").removeAttribute("hidden");
     return false;
 }
@@ -79,3 +72,26 @@ function check() {
     }
     return true;
 } 
+
+function showNav() {
+    var nav = document.querySelector("#navigation");
+    var linkA = document.createElement("a");
+    var linkA1 = document.createTextNode("Home");
+    linkA.setAttribute("href", "./honesty.html");
+    linkA.appendChild(linkA1);
+    nav.appendChild(linkA);
+
+    var linkB = document.createElement("a");
+    var linkB1 = document.createTextNode("Product");
+    linkB.setAttribute("href", "./product.html");
+    linkB.appendChild(linkB1);
+    nav.appendChild(linkB);
+
+    var linkC = document.createElement("a");
+    var linkC1 = document.createTextNode("Reflection");
+    linkC.setAttribute("href", "./reflection.html");
+    linkC.appendChild(linkC1);
+    nav.appendChild(linkC);
+
+    document.querySelector("#logo").removeAttribute("onclick");
+}
