495f2c7f5b4c13fbe357a178f81c14e85c8a62e2
Francis Colendres <flcolendres@myseneca.ca>
2022-04-10 20:54:23 -0400

    issues with js

--- product.js
+++ product.js
@@ -1,4 +1,12 @@
-function valid() {
-
-    return false;
+function digitOnly() {
+    var product_id = document.signup.productId.value.trim();
+    if(product_id.length != 8) {
+        alert("Product ID: Please enter 8 digits only.")
+        return false;
+    }
+    return true;
 }
+
+function check() {
+    return digitOnly();
+} 
